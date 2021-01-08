8253950cd38aed8de5cc78a7988dd1d79ab0d7d3
c4rt0 <c4rt0gr4ph3r@gmail.com>
2021-01-08 20:52:11 +0000

    Modified map component of HillfortMapActivity - added markers

--- app/src/main/java/org/wit/placemark/activities/HillfortMapsActivity.kt
+++ app/src/main/java/org/wit/placemark/activities/HillfortMapsActivity.kt
@@ -2,12 +2,17 @@ package org.wit.placemark.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import org.wit.placemark.R
 import kotlinx.android.synthetic.main.activity_hillfort_maps.*
+import org.wit.placemark.main.MainApp
 
 class HillfortMapsActivity : AppCompatActivity() {
 
+    lateinit var app: MainApp
     lateinit var map: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +20,8 @@ class HillfortMapsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_hillfort_maps)
         setSupportActionBar(toolbar)
         mapView.onCreate(savedInstanceState);
+        app = application as MainApp
+
         mapView.getMapAsync {
             map = it
             configureMap()
@@ -23,8 +30,13 @@ class HillfortMapsActivity : AppCompatActivity() {
 
     fun configureMap() {
         map.uiSettings.setZoomControlsEnabled(true)
+        app.hillforts.findAll().forEach {
+            val loc = LatLng(it.lat, it.lng)
+            val options = MarkerOptions().title(it.title).position(loc)
+            map.addMarker(options).tag = it.id
+            map.moveCamera(CameraUpdateFactory.newLatLngZoom(loc, it.zoom))
+        }
     }
-
     override fun onDestroy() {
         super.onDestroy()
         mapView.onDestroy()
